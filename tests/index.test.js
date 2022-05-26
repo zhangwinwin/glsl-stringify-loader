4a1ab55eec0133a24a001fd1f1feb92960c7ac2b
zhangwinwin <344879340@qq.com>
2022-05-26 23:38:44 +0800

    refactor:  * 测试通过

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,10 +1,9 @@
-const wepback = require('../utils/readWebpack')
+const webpack = require('../utils/readWebpack')
 jest.setTimeout(10000);
 describe('glsl-loader', () => {
-    test('glsl file with local & external dependencies', () => {
-        return wepback('config.js', 'entry.js').then(data => {
-            expect(data.indexOf('#define GLSLIFY 1')).toBeTruthy();
-            expect(data.indexOf('vec3 hello =')).toBeTruthy();
-        })
+    test('glsl file with local & external dependencies', async () => {
+        const stats = await webpack('config.js', 'entry.js');
+        expect(stats.indexOf('#define GLSLIFY 1')).toBeTruthy();
+        expect(stats.indexOf('vec3 hello =')).toBeTruthy();
     })
 })
