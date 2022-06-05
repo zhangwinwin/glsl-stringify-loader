ca78edc33e9d7e5a2d3b96d0e9fb95dee567ef8a
zhangwinwin <344879340@qq.com>
2022-06-05 15:49:07 +0800

    refactor: * 优化测试用例

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,9 +1,9 @@
 const webpack = require('../utils/readWebpack')
 jest.setTimeout(10000);
-describe('glsl-loader', () => {
+describe('glsl-stringify-loader', () => {
     test('glsl file with local & external dependencies', async () => {
         const stats = await webpack('config.js', 'entry.js');
-        expect(stats.indexOf('#define GLSLIFY 1')).toBeTruthy();
+        expect(stats.indexOf('gl_FragColor =')).toBeTruthy();
         expect(stats.indexOf('vec3 hello =')).toBeTruthy();
     })
 })
